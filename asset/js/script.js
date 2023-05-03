aaea719c6dd495c8f4d019528cf6615e14d6c5fa
AriefDr <ariefdwir16@gmail.com>
2023-05-03 15:46:52 +0700

    add bootacm project, add skills, certificate

--- asset/js/script.js
+++ asset/js/script.js
@@ -45,7 +45,7 @@ $(document).ready(function(){
     // typing text animation script
 
     var typed = new Typed(".typing", {
-        strings: ["Fresh Graduate", 'Data Scientist' , "Web Developer"],
+        strings: ["Fresh Graduate", 'Data Scientist' , "Data Analyst", "Data Engineer", "Software Engineering"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
